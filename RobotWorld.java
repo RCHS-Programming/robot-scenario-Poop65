ba420bf189d93fea7d9457af96e55b1e76b3a570
Poop65 <82196810+Poop65@users.noreply.github.com>
2021-05-19 15:42:51 -0400

    Part 3

--- RobotWorld.java
+++ RobotWorld.java
@@ -14,5 +14,20 @@ public class RobotWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        addObject(new Robot(), 50, 50);
+        addObject(new Wall(), 50, 150);
+        addObject(new Wall(), 150, 150);
+        addObject(new Wall(), 250, 150);
+        addObject(new Block(), 405, 150);
+        addObject(new Wall(), 555, 150);
+        addObject(new Wall(), 655, 150);
+        addObject(new Wall(), 755, 150);
+        addObject(new Home(), 750, 550);
+        addObject(new ScorePanel(), 75, 550);
+        addObject(new Pizza(), 300, 50);
+        addObject(new Pizza(), 600, 50);
+        addObject(new Pizza(), 300, 400);
+        addObject(new Pizza(), 600, 400);
+        addObject(new Pizza(), 450, 550);
     }
 }
